9b665e4a3c9dce31fda0854c197807f7eb5c167c
admin <admin@172-16-180-174.DYNAPOOL.NYU.EDU>
2017-09-19 15:09:57 -0400

    what do you want for dinner type your answer

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-alert("scripts.js has loaded!");
+let userinput;
+userinput = prompt("what do you want to have for dinner?", "type your answer here.");
+if (userinput === "burrito") {
+  $("#response").html("brilliant choice!");
+} else {
+  $("#reponse").html("dont you want a burrito?");
+}
