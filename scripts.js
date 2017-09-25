7ba27289ebdca68f735437b29f5e5d65a5e05983
admin <admin@admins-MacBook-Pro-9.local>
2017-09-25 15:43:38 -0400

    Exercise ch. 4

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-let tipcalculator
-tipcalculator= function(total, tiprate){
+let tipcalculator;
+tipcalculator=function(total, tiprate){
   //step 1:
   let tipamount;
-  tipamount = tiprate*total;
-  //and step 2:
-  $("#response").html("your tip is $" + tipamount);
+  tipamount=tiprate * total;
+  // and step 2:
+  $("#response").html("your tip is $"+tipamount);
 }
 
 //now call (or "execute") the function, passing a
-// total of $50.00 and a tiprate of 20%
+//total of $50.00 and a tiprate of .2
 
 tipcalculator(50.00, 0.2);
