3595d5eddea73f274d1bdbf9b188a4b0e6efc052
admin <admin@admins-MacBook-Pro-9.local>
2017-09-25 15:16:07 -0400

    Tip calculator

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-for (let i=1; i<4; i+1) {
-  $("#response").append("<br />" +i)
+let tipcalculator
+tipcalculator= function(total, tiprate){
+  //step 1:
+  let tipamount;
+  tipamount = tiprate*total;
+  //and step 2:
+  $("#response").html("your tip is $" + tipamount);
 }
+
+//now call (or "execute") the function, passing a
+// total of $50.00 and a tiprate of 20%
+
+tipcalculator(50.00, 0.2);
