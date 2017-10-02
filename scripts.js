39045ba5416a3051592cb6c778e656be4a13d04d
admin <admin@172-18-161-8.DYNAPOOL.NYU.EDU>
2017-10-02 18:36:16 -0400

    exercises

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-let string, firstletter, stringlength;
-string="this is a string.";
-//strings have indices and lengths, just like arrays:
-firstletter=string[0];
-//firstletter is "T"
-stringlength=string.length;
-//stringlength is 17
-//
-//strings also have methods, just like arrays:
-let uppercasestring, replacedstring;
-uppercasestring=string.touppercase();
-//uppercasestring is "THIS IS A STRING."
-replacedstring=string.replace("string","pipe");
-//replacedstring is "this is a pipe."
+let arrayofstrings, arrayofnumbers;
+arrayofstrings=["a","b","c"];
+$("#response").html(arrayofstrings[2]);
