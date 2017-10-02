5e64241f0ae2ca7f656aaa47b3748da8ff8cf1dd
admin <admin@172-18-161-8.DYNAPOOL.NYU.EDU>
2017-10-02 18:24:44 -0400

    methods #2

--- scripts.js
+++ scripts.js
@@ -1,24 +1,15 @@
-let myhabanerosaucesquirts, myburritoobject;
-//first, define and assign a variable for how
-//spicy the burrito is.
-myhabanerosaucesquirts= 3;
-//now assign the burrito object.
-myburritoobject={
-  tortilla: "wheat",
-  quacamole: true,
-  beans: "pinto",
-  //make use of the variable above.
-  habanerosaucesquirts: myhabanerosaucesquirts,
-  //use the variable again in a function.
-  spiciness: function(){
-    if (myhabanerosaucesquirts>0){
-      alert("this is a spicy burrito!");
-    }else{
-      alert("this is a mild burrito.");
-    }
-  }
-};
-$("#response").html("your burrito has" +
-  myburritoobject.habanerosaucesquirts+
-  "squirts of habanero.");
-myburritoobject.spiciness();
+let turtles, sortedturtles, reversedturtles, turtlenames;
+turtles= ["leonardo", "donatello", "raphael", "michelangelo"];
+sortedturtles= turtles.sort();
+//sortedturtles is:
+//["donatello","leonardo","michelangelo","raphael"]
+reversedturtles=turtles.reverse();
+//reversedturtles is:
+//["raphael","michelangelo","leonardo","donatello"]
+turtlenames=turtles.join(" ");
+//turtlesnames is "leonardo donatello raphael michelangelo"
+turtles.push("splinter");
+//turtles is now:
+//["leonardo","donatello","raphael","michelangelo","splinter"]
+turtles.pop();
+//back to ["leonardo","donatello","raphael","michelangelo"]
