395840b057051b2514a8e3fbf97aa5c051c7bbb6
admin <admin@172-18-161-8.DYNAPOOL.NYU.EDU>
2017-10-02 18:16:39 -0400

    methods

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
-let myburritoobject;
-myburritoobject= {
+let myhabanerosaucesquirts, myburritoobject;
+//first, define and assign a variable for how
+//spicy the burrito is.
+myhabanerosaucesquirts= 3;
+//now assign the burrito object.
+myburritoobject={
   tortilla: "wheat",
-  guacamole: true,
+  quacamole: true,
   beans: "pinto",
-  habanerosaucesquirts: 3
+  //make use of the variable above.
+  habanerosaucesquirts: myhabanerosaucesquirts,
+  //use the variable again in a function.
+  spiciness: function(){
+    if (myhabanerosaucesquirts>0){
+      alert("this is a spicy burrito!");
+    }else{
+      alert("this is a mild burrito.");
+    }
+  }
 };
-//and let's access a property
-$("#response").html(myburritoobject.tortilla);
+$("#response").html("your burrito has" +
+  myburritoobject.habanerosaucesquirts+
+  "squirts of habanero.");
+myburritoobject.spiciness();
