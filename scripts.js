12f5abf4eee3e03585e1fd4ec20cd83917d09e94
admin <admin@172-18-164-240.DYNAPOOL.NYU.EDU>
2017-10-09 18:25:57 -0400

    exercise #3

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
-let leonardo, donatello, raphael, michelangelo, turtles, weapons;
-leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
-donatello = {name: "Donatello", color: "purple", weapon: "bo"};
-raphael = {name: "Raphael", color: "red", weapon: "sai"};
-michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
-turtles = [leonardo, donatello, raphael, michelangelo];
-weapons = turtles.map(function(turtle){
-  return turtle.weapon;
-}).sort().join(", "+"and ");
-$("#response").html(weapons);
-//This makes it so every weapon has
-//and in front of it, but I'm not sure
-//how to get it so only sai has and in front of it
+if(turtle.name.includes("o") ){
+  return turtle.name;
+}
+//turtles and their names have not been defined
+// and maps are utilized so we can build arrays out of other arrays
+// if's behave in a certain way based on a condition  
