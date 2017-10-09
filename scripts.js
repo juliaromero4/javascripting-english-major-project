468470df63398cc80323a407157073d41f8204dc
admin <admin@172-18-164-240.DYNAPOOL.NYU.EDU>
2017-10-09 18:00:25 -0400

    exerscise #1

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
-let leonardo, donatello, raphael, michelangelo, turtles, weapons;
-leonardo={name:"leoanrdo",color:"blue",weapon:"katana"};
-donatello={name:"donatello", color:"purple",weapons:"bo"};
-raphael={name:"raphael", color:"red",weapon:"sai"};
-michelangelo={name:"michelangelo",color:"blue",weapon:"nuchaku"};
-turtles=[leonardo, donatello, raphael, michelangelo];
-weapons=turtles.map(function(turtle){
-  return turtle.weapon;
-}).sort().join(", ");
-$("#response").html(weapons);
+let numbers, newnumbers;
+numbers=[1,2,3];
+newnumbers=numbers.map(function(number){
+  return number * 2;
+});
+console.log("the doubled numbers are", newnumbers);
