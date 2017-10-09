9afd38aedc2905b50e42bf90ff46731965428db2
admin <admin@172-18-164-240.DYNAPOOL.NYU.EDU>
2017-10-09 18:18:15 -0400

    exercise #2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
-let numbers, newnumbers;
-numbers=[1,2,3];
-newnumbers=numbers.map(function(number){
-  return number * 2;
-});
-console.log("the doubled numbers are", newnumbers);
+let leonardo, donatello, raphael, michelangelo, turtles, weapons;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo];
+weapons = turtles.map(function(turtle){
+  return turtle.weapon;
+}).sort().join(", "+"and ");
+$("#response").html(weapons);
+//This makes it so every weapon has
+//and in front of it, but I'm not sure
+//how to get it so only sai has and in front of it
