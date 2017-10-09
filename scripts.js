81c24bedd28ba3235d2e33b43cdab57e450abf9f
admin <admin@172-18-164-240.DYNAPOOL.NYU.EDU>
2017-10-09 17:10:49 -0400

    iterating

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-let arrayofstrings, arrayofnumbers;
-arrayofstrings=["a","b","c"];
-$("#response").html(arrayofstrings[2]);
+let userstring, uppercaseminusE, uppercasedstring;
+userstring=prompt("what do you want to uppercase?");
+uppercaseminusE=function(string){
+  for(let i= 0; i<string.length; i=i+1){
+    let result;
+    result="";
+    for(let i=0; i<string.length; i=i+1) {
+    let letter;
+    letter=string[i];
+    if (letter==="e"){
+      result=letter;
+    }else {
+      result=letter.touppercase();
+    }
+  }
+  return result;
+}
+};
+uppercasedstring=uppercaseminusE(userstring);
+$("#response").html(uppercasedstring);
