6cc9adbed02fb901a07c316a9ea75f40f205b218
admin <admin@172-18-166-162.DYNAPOOL.NYU.EDU>
2017-10-24 15:18:25 -0400

    exercise #1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,1 @@
-if(turtle.name.includes("o") ){
-  return turtle.name;
-}
-//turtles and their names have not been defined
-// and maps are utilized so we can build arrays out of other arrays
-// if's behave in a certain way based on a condition  
+
