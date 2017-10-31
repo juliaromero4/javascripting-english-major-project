7817a70eb3fc6879c9bbed8f8dea71bf793e0e72
admin <admin@172-18-165-151.DYNAPOOL.NYU.EDU>
2017-10-31 19:02:12 -0400

    exercises

--- prologue.js
+++ prologue.js
@@ -1,57 +1,33 @@
 
-$("#prologue").html("<p>The text of the Prologue will go here.</p>");
-$("#glosses").html("<p>The glosses will go here.</p>");
-let line1, line1Text; // don’t need the intermediate step of line1TextArray
-line1 = [{text: "Whan"}, {text: "that"}, {text: "Aprill,"}, {text: "with"},
-        {text: "his"}, {text: "shoures"}, {text: "soote"}];
-line1Text = line1.map(function(word){
-  return word.text;
-}).join(" ");
-$("#prologue").html("<p>" + line1Text + "<br /></p>");
-  line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
-          modern: "April,"}, {text: "with"}, {text: "his"}, {text: "shoures",
-          modern: "showers"}, {text: "soote", modern: "sweet"}];
+$.getJSON("prologue.json", function(data){ // Note the data variable!
+  let prologueText; // Define the variable you didn’t need before.
+  prologueText = "<blockquote><p>"; // Open the tags.
+  // Now you can iterate over the data variable’s .lines property:
+  data.lines.forEach(function(line){ // We get a variable, line.
+    // Define a blank lineText.
+    let lineText;
+    lineText = "";
 
-  line1Text = "<blockquote><p>";
-        line1.forEach(function(word){
-
-  line1Text = line1Text + word.text + " ";
-        });
-        line1Text = line1Text + "<br />(line 2 would go here)</p></blockquote>";
-        $("#prologue").html(line1Text);
-line1.foreach(function(word){
-  let wordstring;
-  wordstring=word.text;
-  if (word.modern){
-    wordstring="<a href='#'>"+wordstring+"</a>";}
-  line1Text=line1Text+wordstring+" ";
-});
-$("#prologue a").click(function(){
-  $("#prologue a").click(function(){
-    // Define the text and the word that was clicked.
-    let glossText, clickedWord;
-    clickedWord = $( this ).text();
-    glossText = "<h2>You clicked on the word: " + clickedWord + "</h2>";
-    $("#glosses").html(glossText);
+    line.forEach(function(word){
+      let wordString;
+      wordString = word.text;
+      if (word.modern){
+        wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
+      }
+      lineText = lineText + wordString + " ";
+    });
+    // Add lineText with a line break to the prologueText.
+    prologueText = prologueText + lineText + "<br/>";
   });
-  $("#glosses").append("<h2>You clicked on a word!</h2>");
+  // Close the prologueText tags.
+  prologueText = prologueText + "</p></blockquote>";
+  // Replace the content of #prologue.
+  $("#prologue").html(prologueText);
+}
+setTheDefaultGlossesValue();
+setTheDefaultPrologueValue(function(){
+  selectThePrologueLinksAndWaitForClicks();
 });
-line1.forEach(function(word){
-  let wordString;
-  wordString = word.text;
-  if (word.modern){
-    // Add word.modern as a data attribute to the <a> tag.
-    wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
-  }
-  line1Text = line1Text + wordString + " ";
-});
-$("#prologue a").click(function(){
-  let glossText, clickedWord, modernWord;
-  clickedWord = $( this ).text();
-  // .data("modern") looks for the data-modern HTML attribute.
-  modernWord = $( this ).data("modern");
-  glossText = "<h2>You clicked on " + clickedWord + ", which means " + modernWord +"</h2>";
-  $("#glosses").html(glossText);
-});
-//I copied and pasted all of your given sets of code and
-//I still am not getting any result on my page 
+//we can not do $(this).modern to get the .modern property because modern is
+//a function of text so we have to reference text
+// I have been trying to answer excercises 1 and 2 and I am very confused. 
