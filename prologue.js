3b688d589a12e2dc8e78acced17f106461f43c66
admin <admin@172-18-165-151.DYNAPOOL.NYU.EDU>
2017-10-31 17:52:55 -0400

    first line of prologue

--- prologue.js
+++ prologue.js
@@ -1,2 +1,10 @@
 $("#prologue").html("<p>The text of the Prologue will go here.</p>");
 $("#glosses").html("<p>The glosses will go here.</p>");
+$("#glosses").html("<p>The glosses will go here.</p>");
+let line1, line1Text;
+line1 = [{text: "Whan"}, {text: "that"}, {text: "Aprill,"}, {text: "with"},
+        {text: "his"}, {text: "shoures"}, {text: "soote"}];
+line1Text = line1.map(function(word){
+  return word.text;
+}).join(" ");
+$("#prologue").html("<p>" + line1Text + "<br /></p>");
